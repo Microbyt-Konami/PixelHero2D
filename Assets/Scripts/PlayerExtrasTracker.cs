c13e87c405c4bf9f79ceb7d0ffb8061cdd0ec606
bugbit <ohernandezba@gmail.com>
2023-12-27 16:52:47 +0100

    Desbloqueo genérico de EXTRAS

--- Assets/Scripts/PlayerExtrasTracker.cs
+++ Assets/Scripts/PlayerExtrasTracker.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PlayerExtrasTracker : MonoBehaviour
 {
-    [SerializeField] private bool _canDoubleJump, _canDash, _CanEnterBallMode, _CanDropBombs;
+    // Fields
+    [SerializeField] private bool _canDoubleJump, _canDash, _canEnterBallMode, _canDropBombs;
 
     public bool CanDoubleJump { get => _canDoubleJump; set => _canDoubleJump = value; }
     public bool CanDash { get => _canDash; set => _canDash = value; }
-    public bool CanEnterBallMode { get => _CanEnterBallMode; set => _CanEnterBallMode = value; }
-    public bool CanDropBombs { get => _CanDropBombs; set => _CanDropBombs = value; }
+    public bool CanEnterBallMode { get => _canEnterBallMode; set => _canEnterBallMode = value; }
+    public bool CanDropBombs { get => _canDropBombs; set => _canDropBombs = value; }
 }
