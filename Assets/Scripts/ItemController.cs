d5061eeabfba248403b2092eb7522c447fbfca7e
bugbit <ohernandezba@gmail.com>
2023-12-28 10:51:37 +0100

    CoinSpin implementacion

--- Assets/Scripts/ItemController.cs
+++ Assets/Scripts/ItemController.cs
@@ -8,7 +8,18 @@ public class ItemController : MonoBehaviour
     [SerializeField] private float waitForHide;
 
     // Components
+    private SpriteRenderer sr;
+    private Transform transformItem;
     private ItemsManager itemsManager;
+
+    // Variables
+    private bool isHiding;
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        transformItem = GetComponent<Transform>();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +29,27 @@ public class ItemController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
-            itemsManager.CatchIt(this);
+        if (collision.CompareTag("Player") && !isHiding)
+            StartCoroutine(CatchIt());
+    }
+
+    private IEnumerator CatchIt()
+    {
+        isHiding = true;
+        itemsManager.CatchIt(this);
+
+        var position = transformItem.position;
+        var color = sr.color;
+        var _waitForHide = waitForHide;
+
+        while (waitForHide > 0)
+        {
+            waitForHide -= Time.deltaTime;
+            transformItem.position = position + (2 - 2 * Mathf.Lerp(0, _waitForHide, waitForHide)) * Vector3.up;
+            sr.color = new Color(color.r, color.g, color.b, Mathf.Lerp(0, _waitForHide, waitForHide));
+
+            yield return null;
+        }
+        Destroy(gameObject);
     }
 }
