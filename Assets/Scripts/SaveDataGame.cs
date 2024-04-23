aea6c4edb4e63334a3743375bebecc15410c7ffa
Oscar Devs <ohernandezba@gmail.com>
2024-04-23 23:17:39 +0200

    Update SaveDataGame.cs

--- Assets/Scripts/SaveDataGame.cs
+++ Assets/Scripts/SaveDataGame.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Security.Cryptography;
 using UnityEngine;
 
 using Newtonsoft.Json.Linq;
 using UnityEngine.Scripting;
+using System;
+using System.Text;
 
 public class SaveDataGame : MonoBehaviour
 {
+
+    byte[] key = { 92, 149, 74, 167, 16, 8, 60, 7, 187, 86, 156, 215, 179, 194, 146, 89, 99, 103, 181, 126, 196, 223, 67, 218, 120, 47, 234, 144, 64, 133, 235, 141 };
+    byte[] iVector = { 245, 94, 116, 78, 187, 235, 49, 7, 213, 231, 91, 248, 26, 63, 174, 12 };
+
+
     private List<ISerializable> _objectsToSerialize;
     private List<string> itemsGOCatched = new List<string>();
 
@@ -26,6 +34,31 @@ public class SaveDataGame : MonoBehaviour
     void Awake()
     {
         _objectsToSerialize = new List<ISerializable>();
+
+        /*
+        AesManaged aesManaged = new AesManaged();
+
+        aesManaged.GenerateKey();
+        aesManaged.GenerateIV();
+
+        print("Random key:");
+        PrintByteArray(aesManaged.Key);
+
+        Console.WriteLine("\nRandom iVector:");
+        PrintByteArray(aesManaged.IV);
+
+        void PrintByteArray(byte[] byteArray)
+        {
+            var sb = new StringBuilder();
+            foreach (byte b in byteArray)
+            {
+                sb.Append(b);
+                sb.Append(',');
+            }
+
+            print(sb.ToString());
+        }
+        */
     }
 
     // Start is called before the first frame update
