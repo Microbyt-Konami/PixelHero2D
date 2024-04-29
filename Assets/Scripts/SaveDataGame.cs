65d2f68b4c921a3d912c1db563d23eba33869264
Oscar Devs <ohernandezba@gmail.com>
2024-04-29 16:08:56 +0200

    Update SaveDataGame.cs

--- Assets/Scripts/SaveDataGame.cs
+++ Assets/Scripts/SaveDataGame.cs
@@ -11,10 +11,8 @@ using System.Text;
 
 public class SaveDataGame : MonoBehaviour
 {
-
     byte[] key = { 92, 149, 74, 167, 16, 8, 60, 7, 187, 86, 156, 215, 179, 194, 146, 89, 99, 103, 181, 126, 196, 223, 67, 218, 120, 47, 234, 144, 64, 133, 235, 141 };
     byte[] iVector = { 245, 94, 116, 78, 187, 235, 49, 7, 213, 231, 91, 248, 26, 63, 174, 12 };
-
 
     private List<ISerializable> _objectsToSerialize;
     private List<string> itemsGOCatched = new List<string>();
@@ -65,7 +63,7 @@ public class SaveDataGame : MonoBehaviour
     IEnumerator Start()
     {
         yield return new WaitForEndOfFrame();
-        // Asegurarse de que los objetos se ha cargado correctamente y por tanto todos los objetos que implementen ISerializable se han cargado
+        // Nos aseguraramos de que los objetos se ha cargado correctamente y por tanto todos los objetos que implementen ISerializable se han cargado
 
         bool isLoadData;
         try
@@ -80,12 +78,6 @@ public class SaveDataGame : MonoBehaviour
         if (!isLoadData)
             ResetData();
     }
-
-    // Update is called once per frame
-    // void Update()
-    // {
-
-    // }
 
     void OnApplicationQuit()
     {
@@ -182,6 +174,5 @@ public class SaveDataGame : MonoBehaviour
         memoryStream.Close();
 
         return decryptedPlainText;
-
     }
 }
