ac84412d1ef2355b27958ff9d58bafd8bc99d34b
Oscar Devs <ohernandezba@gmail.com>
2024-04-29 16:48:11 +0200

    Prototipo Version 1.2

--- Assets/Scripts/ItemsManager.cs
+++ Assets/Scripts/ItemsManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Newtonsoft.Json.Linq;
-using UnityEditor.Profiling.Memory.Experimental;
+//using UnityEditor.Profiling.Memory.Experimental;
 using UnityEngine;
 
 public class ItemsManager : MonoBehaviour, ISerializable
